4f03395afbe4d8f1d71ffb6046b63c725a94e0cc
Anderson correia <40068482+AndersonCorreia@users.noreply.github.com>
2020-03-01 01:21:04 -0300

    Removendo metodo put

--- laravel/resources/views/cadastro.blade.php
+++ laravel/resources/views/cadastro.blade.php
@@ -11,7 +11,7 @@
         <!-- Cadastro Pessoa Fisica -->
         <div class="form-group col-sm-7">
                 <span class="col-1 p-0">Razão Social</span>
-                <input type="hidden" name="_method" value="put">
+                
                 <input id="nomeInst"  class="form-control" type="text" maxlength="40" name="razaoSocial" placeholder="Insira a razão social da empresa" 
                 value=""  title="Razão Social" required autofocus>
             </div>
