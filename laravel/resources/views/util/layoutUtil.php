a13fc664427ba5065bd06fb77389eaa864dbf143
Anderson correia <40068482+AndersonCorreia@users.noreply.github.com>
2020-02-29 23:38:26 -0300

    Update layoutUtil.php

--- laravel/resources/views/util/layoutUtil.php
+++ laravel/resources/views/util/layoutUtil.php
@@ -2,8 +2,7 @@
 
 function getMenuLinks(){
     return [//todos os possiveis links do menu, utilizado no layout da area administrativa
-        'inicio'=>   ['link'=>route("inicio") , 'texto'=>'Inicio' ],// texto é o nome que vai estar dentro da tag <a>
         'ACadastro'=>['link'=>route('cadastro.show') , 'texto'=>'Adicionar cadastro de cliente' ],
-        'ECadastro'=>['link'=>route('editarCadastro.show') , 'texto'=>'Editar ou excluir cadastro de cliente' ]
+        'ECadastro'=>['link'=>route('cadastrosVinculados.show') , 'texto'=>'Editar ou excluir cadastro de cliente' ]
     ];
 }
